1c76c6a29dc16f03f9a8db06a3ffc01b2e9f252f
Foggynoti0n <88888936+Foggynoti0n@users.noreply.github.com>
2022-12-22 08:46:48 -0300

    Delete src directory

--- src/utils/CostFunctions.js
+++
@@ -1,9 +0,0 @@
-const Cost= ({valor}) => {
-    const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    });
-    return formatter.format(valor)
-}
-
-export default Cost;
